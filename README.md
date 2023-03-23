373aa95460092de94ff1c9077fb4ee78177d9720
Ryan Po <ryanpo@DNa828d1f.SUNet>
2023-03-22 19:11:49 -0700

    site revamp

--- README.md
+++
@@ -1,1 +0,0 @@
-# ryanpo.github.io
--- assets/js/default.js
+++
@@ -1,5 +0,0 @@
-document.body.className = 'fade';
-document.addEventListener("DOMContentLoaded", function(e) {
-    document.body.classList.remove('fade');
-  });
-
